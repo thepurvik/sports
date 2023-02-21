ca8503ce60c9d04764f0a6a2a3e45da4a82b8e1c
The Devloper <73634258+thepurvik@users.noreply.github.com>
2023-02-21 11:26:36 +0530

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  // api: 'https://dev-webapi.buddybrawl.com/api/',
+  // socket: 'https://dev-webapi.buddybrawl.com/websocket?',
+  api: 'https://webapi.buddybrawl.com/api/',
+  socket: 'https://webapi.buddybrawl.com/websocket?',
+};
